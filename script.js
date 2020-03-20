7a67119341740d80d35faf96fb689af96886ae15
Farid <example@gmail.com>
2020-03-20 22:20:48 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+
+
+// console.log(leftBorderWidth);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+// console.log(persone.name);
+// console.log(persone["age"]);
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = prompt("Are you here?", "Да");
+
+
+
+console.log(typeof (answer));
+let incr = 10,
+    decr = 10;
+    incr++;
+    decr--;
+    console.log(incr, decr);
+     
+ 
+
+
+
+
+
